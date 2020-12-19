aa70205f004e1269c0fd969d14c8ae7f17e6b41f
philong1988 <70334481+philong1988@users.noreply.github.com>
2020-12-19 12:51:15 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.showIcon(IconNames.Giraffe)
+basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Heart)
+basic.showString("Hello!")
+basic.showIcon(IconNames.Tortoise)
+basic.showArrow(ArrowNames.North)
+basic.showIcon(IconNames.House)
+basic.forever(function () {
+	
+})
